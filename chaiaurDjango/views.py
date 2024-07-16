01f3b44439bf92f878e666d90aa65ea6a41a341d
Anshu Singh <anshusingh9907@gmail.com>
2024-07-17 00:18:03 +0530

    template,static,linking,laod,erros
    use of static template views.py URLconf path and linking css in html with template language load static and configuring staticURL

--- chaiaurDjango/views.py
+++ chaiaurDjango/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request): #request will request a url 
     
-    #Http Response will create response to the  
-    return HttpResponse("hey You are at the home page ")
+    #Http Response will create response to the
+    # return HttpResponse("hey You are at the home page ")
+    return render(request, 'website/index.html')
 
 def About(request):
-    return HttpResponse("hey You are at the About page ")
+    return render(request, 'website/About.html')
+    
 
 def Contact(request):
-    return HttpResponse("hey You are at the Contact page ")
+    return render(request, 'website/contact.html')
 
 #these are all methods created for the pages
 
